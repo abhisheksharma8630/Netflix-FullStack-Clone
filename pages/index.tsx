cd3d6423ebcb8acfc4367c239a0257ba889f9443
Abhishek Sharma <abhishek.sharma_cs22@gla.ac.in>
2024-01-18 12:02:14 +0530

    update authorisation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,31 @@
+import useCurrentUser from "@/hooks/useCurrentUser";
+import { NextPageContext } from "next";
+import { getSession, signOut } from "next-auth/react";
+
+
+export async function getServerSideProps(context:NextPageContext) {
+  const session = await getSession(context);
+  if(!session){
+    return {
+      redirect:{
+        destination:'/auth',
+        permanent: false,
+      }
+    }
+  }
+  return {
+    props:{}
+  }
+}
+
 export default function Home() {
+  const {data:user} = useCurrentUser();
+
   return (
-    <div className='bg-red-400'>Hello Netflix Clone</div>
+    <div>
+      <p className='bg-red-400'>Netflix Clone</p>
+      <p className="bg-white">Logged in as : {user?.name}</p>
+      <button className="bg-white" onClick={()=>signOut()}>Logout</button>
+    </div>
   )
 }
